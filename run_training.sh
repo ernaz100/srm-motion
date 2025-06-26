0c90b22bc960786906af1b752b5a49d64984bb27
ernaz100 <nazarenusen@gmail.com>
2025-06-26 18:35:53 +0200

    sbatch file

---
+++ run_training.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Sample Slurm job script for Galvani 
+
+#SBATCH -J spatial-reasoning-motion-diffusion                # Job name
+#SBATCH --ntasks=1                 # Number of tasks
+#SBATCH --cpus-per-task=8          # Number of CPU cores per task
+#SBATCH --nodes=1                  # Ensure that all cores are on the same machine with nodes=1
+#SBATCH --partition=a100-galvani   # Which partition will run your job
+#SBATCH --time=2-23:59             # Allowed runtime in D-HH:MM
+#SBATCH --gres=gpu:1               # (optional) Requesting type and number of GPUs
+#SBATCH --mem=100G                  # Total memory pool for all cores (see also --mem-per-cpu); exceeding this number will cause your job to fail.
+#SBATCH --output=/mnt/lustre/work/ponsmoll/pba794/spatial-reasoning-motion-diffusion/logs/myjob-%j.out       # File to which STDOUT will be written - make sure this is not on $HOME
+#SBATCH --error=/mnt/lustre/work/ponsmoll/pba794/spatial-reasoning-motion-diffusion/logs/myjob-%j.err        # File to which STDERR will be written - make sure this is not on $HOME
+#SBATCH --mail-type=ALL            # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=eric.nazarenus@student.uni-tuebingen.de   # Email to which notifications will be sent
+
+# Diagnostic and Analysis Phase - please leave these in.
+scontrol show job $SLURM_JOB_ID
+pwd
+nvidia-smi # only if you requested gpus
+ls $WORK # not necessary just here to illustrate that $WORK is available here
+
+# Setup Phase
+# add possibly other setup code here, e.g.
+# - copy singularity images or datasets to local on-compute-node storage like /scratch_local
+# - loads virtual envs, like with anaconda
+# - set environment variables
+# - determine commandline arguments for `srun` calls
+conda activate $WORK/.conda/srm/
+# Compute Phase
+srun bash train.sh ms_tiny   
